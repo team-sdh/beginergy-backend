c3118933f13c8ba055f1ae4c73c9af799d4f42a7
hanhqvu <hanhqvu@users.noreply.github.com>
2023-06-04 15:48:14 +0900

    refactor: return data for chart

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -28,12 +28,15 @@ func routes(_ app: Application) throws {
             let config = MLModelConfiguration()
             let model = try KrakenRegressor(configuration: config)
             
-            var halfHourPredictions: [[String: Double]] = []
+            var halfHourPredictions: [[String:Double]] = []
 
             for index in 0...23 {
-                let prediction1 = try model.prediction(Day: day, Start: Double(index), End: Double(index) + 0.5)
-                let predictionRead1 = [String(index): prediction1.Value]
-                halfHourPredictions.append(predictionRead1)
+                var prediction1: [String:Double] = [:]
+                prediction1["x"] = Double(index)
+                prediction1["y"] = try model.prediction(Day: day, Start: Double(index), End: Double(index) + 0.5)
+                halfHourPredictions.append(prediction1)
+                
+                var prediction2: [String:Double] = [:]
                 let prediction2 = try model.prediction(Day: day, Start: Double(index) + 0.5, End: Double(index + 1))
                 let predictionRead2 = [String(Double(index) + 0.5): prediction2.Value]
                 halfHourPredictions.append(predictionRead2)
